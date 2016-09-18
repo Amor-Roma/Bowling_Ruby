442da6b68070009cf3c12c2c456c17674f21c5c2
Chris Roma <chrisroma23@gmail.com>
2016-09-18 16:37:27 -0400

    changed names to better ones

--- game.rb
+++ game.rb
@@ -4,6 +4,7 @@ class Game
   def initialize
     @rolls = Array.new()
     @current_roll = 0
+    @total_score = 0
   end
 
   def roll(pins)
@@ -12,29 +13,32 @@ class Game
   end
 
   def score
-    total_score = 0
-    first_in_frame = 0
+    current_ball = 0
     10.times do
-      if(is_strike(first_in_frame))
-        total_score += strike_frame_score(first_in_frame)
-        first_in_frame += 1
-      elsif(is_spare(first_in_frame))
-        total_score += spare_frame_score(first_in_frame)
-        first_in_frame += 2
+      if(is_strike(current_ball))
+        strike_frame_score(current_ball)
+        current_ball += 1
+      elsif(is_spare(current_ball))
+        spare_frame_score(current_ball)
+        current_ball += 2
       else
-        total_score += @rolls[first_in_frame] + @rolls[first_in_frame + 1]
-        first_in_frame += 2
+        no_mark_frame_score(current_ball)
+        current_ball += 2
       end
     end
-    total_score
+    @total_score
+  end
+
+  def no_mark_frame_score(first_in_frame)
+    @total_score += @rolls[first_in_frame] + @rolls[first_in_frame + 1]
   end
 
   def spare_frame_score(first_in_frame)
-    10 + @rolls[first_in_frame + 2]
+    @total_score += 10 + @rolls[first_in_frame + 2]
   end
 
   def strike_frame_score(first_in_frame)
-    10 + @rolls[first_in_frame + 1] + @rolls[first_in_frame + 2]
+    @total_score += 10 + @rolls[first_in_frame + 1] + @rolls[first_in_frame + 2]
   end
 
   def is_strike(first_in_frame)
